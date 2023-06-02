ab67513fbd4fbf0a957037937fe85a917562b0b4
Brian Klemm <resetbrian@gmail.com>
2023-06-02 09:24:07 -0700

    added error checking for the write function and announcing the number of stored primes before the user request primes.

--- main.cpp
+++ main.cpp
@@ -21,20 +21,25 @@ bool isPrime(int n) {
 
 int main() {
 
-    std::cout << "Enter how many primes you would like me to list." << std::endl;
-    int primes_requested;
-    std::cin >> primes_requested;
-
     std::vector<int> primesFound;
     std::ifstream file_in("primes.dat", std::ios::in);
-    int prime;
+    if (!file_in) {
+        std::cerr << "Error opening file for reading\n";
+        return 1;
+    }
 
-    // Read primes from file
+    int prime;
     while (file_in >> prime) {
         primesFound.push_back(prime);
     }
-
     file_in.close();
+
+    // Tell user how many primes are already saved
+    std::cout << "There are " << primesFound.size() << " primes already saved." << std::endl;
+
+    std::cout << "Enter how many primes you would like me to list." << std::endl;
+    int primes_requested;
+    std::cin >> primes_requested;
 
     int i = primesFound.empty() ? 2 : primesFound.back() + 1;
     while (primesFound.size() < primes_requested) {
@@ -46,10 +51,18 @@ int main() {
 
     // Save primes to file
     std::ofstream file_out("primes.dat", std::ios::out | std::ios::trunc);
+    if (!file_out) {
+        std::cerr << "Error opening file for writing\n";
+        return 1;
+    }
     for(int prime : primesFound) {
         file_out << prime << std::endl;
     }
-
+    file_out.flush(); // flush the buffer to make sure all data is written to the file
+    if (!file_out) {
+        std::cerr << "Error writing to file\n";
+        return 1;
+    }
     file_out.close();
 
     for(int i = 0; i < primes_requested; i++){
